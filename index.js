6151c4caf8d597322895e7466b86008b96971034
enRamirez <ricky.ramirez125042@gmail.com>
2023-07-19 16:20:26 -0500

    Finished towers

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
-let stone = null
+let stone = null;
 
-// this function is called when a Column is clicked. 
+// this function is called when a row is clicked.
 // Open your inspector tool to see what is being captured and can be used.
-const selectColumn = (column) => {
-  const currentColumn = column.getAttribute("data-Column")
-  
-  console.log("Yay, we clicked an item", column)
-  console.log("Here is the stone's id: ", column.id)
-  console.log("Here is the stone's data-size: ", currentColumn)
-  // if i have a stone in my hand already, i must drop it first
-  // if my hand is empty, then i will need to pick up a stone
-  if (!stone) {
-      pickUpStone(column.id)
-    } else {
-      dropStone(column.id)
-    }
-} 
+const selectRow = (row) => {
+  const currentRow = row.getAttribute("data-row");
+  // console.log(`CurrentRow: ${currentRow}`);
+  // console.log(`row: `, row);
+  console.log(`row.id will get passed to next function: ${row.id}`);
+  if (stone === null) {
+    pickUpStone(row.id);
+  } else {
+
+    dropStone(row.id, stone);
+  }
+};
 
 // this function can be called to get the last stone in the stack
 // but there might be something wrong with it...
-const pickUpStone = (columnID) => {
-  const selectedColumn = document.getElementById(columnID);
-  stone = selectedColumn.lastElementChild;
-  selectedColumn.removeChild(stone);
-  console.log(stone)
-}
+const pickUpStone = (rowID) => {
+  const selectedRow = document.getElementById(rowID);
+  // console.log(selectedRow);
+  // console.log(selectedRow.lastElementChild);
+  if (selectedRow.lastElementChild) {
+    stone = selectedRow.removeChild(selectedRow.lastElementChild);
+    console.log(stone);
+    // console.log(`after removal, last element: `, selectedRow.lastElementChild);
+  } else {
+    console.log(`no stones here`);
+  }
+};
 
-// You could use this function to drop the stone but you'll need to toggle between pickUpStone & dropStone
-// Once you figure that out you'll need to figure out if its a legal move...
-// Something like: if(!stone){pickupStone} else{dropStone}
 
-const dropStone = (columnID, stone) => {
-  document.getElementById(columnID).appendChild(stone)
-  stone = null
+const dropStone = (rowID, selectedStone) => {
+  
+  const currentRowElement = document.getElementById(rowID);
+  // console.log(`selectedStone: ` + selectedStone.id);
+  // console.log(!currentRowElement.lastElementChild);
+  if (!currentRowElement.lastElementChild) {
+    currentRowElement.appendChild(selectedStone);
+  } else if (
+    Number(currentRowElement.lastElementChild.id) > Number(selectedStone.id)
+  ) {
+    console.log(`in number comparison area`);
+    document.getElementById(rowID).appendChild(selectedStone);
+  } else {
+    console.log(`invalid move`);
+    return;
+  }
+  // check for win condition
+  // elements start on bottom row, so check middle and top rows for win condition
+  // use document.getElementById("top-row").childElementCount;
+  const topCount = document.getElementById("top-row").childElementCount;
+  const midCount = document.getElementById("middle-row").childElementCount;
+  // console.log(topCount, typeof topCount);
+  // console.log(midCount, typeof midCount);
+  if (topCount === 4 || midCount === 4) {
+    console.log(`YOU WON!!!!`);
+    won();
+  }
+
+  stone = null;
+};
+
+function won() {
+  window.alert(`Congratulations, you won!!!`);
 }
 
 // * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
-
